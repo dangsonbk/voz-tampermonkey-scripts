298d856c822300dc0ab4a52a548a0152265c3e44
son.nguyen <son.nguyen@lge.com>
2020-05-08 09:40:12 +0700

    Remove message footer

---
+++ remove-message-footer.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Voz remove footer
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Remove message footer in voz /t
+// @author       https://github.com/dangsonbk
+// @match        https://next.voz.vn/t/*
+// @updateURL    https://raw.githubusercontent.com/dangsonbk/voz-tampermonkey-scripts/master/remove-message-footer.js
+// @grant        none
+// ==/UserScript==
+
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+
+(function() {
+    'use strict';
+
+    var CSS = '\
+    .message-footer {display: none;} \n \
+          ';
+    addGlobalStyle(CSS);
+})();
